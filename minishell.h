e68b171520dfc057a0a72972f4e07eabed6718cc
TGriffit <tgriffit@student.42lyon.fr>
2022-04-19 18:03:37 +0200

    Libft added + minishell.h updated

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: tgriffit <tgriffit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 15:10:33 by tgriffit          #+#    #+#             */
-/*   Updated: 2022/04/19 15:10:33 by tgriffit         ###   ########.fr       */
+/*   Updated: 2022/04/19 17:53:40 by tgriffit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,16 @@
 # include <readline/history.h>
 
 // FUNCTIONS
+
+// COLORS
+# define BBLU	"\033[1;34m"
+# define BGREEN	"\033[1;32m"
+# define BRED	"\033[1;31m"
+# define BLU	"\033[0;34m"
+# define GRN	"\033[0;32m"
+# define RED	"\033[0;31m"
+# define RST	"\033[0m"
+# define YLW	"\033[93m"
+
 
 #endif
