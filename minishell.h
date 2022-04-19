bc22e6963207bbf66dba4e72a6b432b4b0c79001
TGriffit <tgriffit@student.42lyon.fr>
2022-04-19 15:12:00 +0200

    Header created

---
+++ minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgriffit <tgriffit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 15:10:33 by tgriffit          #+#    #+#             */
+/*   Updated: 2022/04/19 15:10:33 by tgriffit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+// INCLUDES
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+// FUNCTIONS
+
+#endif
