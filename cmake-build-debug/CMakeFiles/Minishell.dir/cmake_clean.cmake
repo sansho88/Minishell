8df713eda0ff1871b96f69daf5b714c92a9ae654
TGriffit <tgriffit@student.42lyon.fr>
2022-04-19 18:02:34 +0200

    Libft added

---
+++ cmake-build-debug/CMakeFiles/Minishell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Minishell.dir/nigga-tests.c.o"
+  "Minishell"
+  "Minishell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Minishell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
