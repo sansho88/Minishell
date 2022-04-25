53ad3db03bd16ac110526eef1485d0cd1bbf3ba0
TGriffit <tgriffit@student.42lyon.fr>
2022-04-25 15:52:55 +0200

    Built-ins: echo implemented

---
+++ built-ins/echo.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgriffit <tgriffit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 15:20:21 by tgriffit          #+#    #+#             */
+/*   Updated: 2022/04/25 15:49:59 by tgriffit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_echo(char *arg)
+{
+	bool	n;
+	char	*tmp;
+
+	n = !ft_strncmp(arg, "-n ", 3);
+	tmp = arg;
+	if (ft_strnstr(arg, "-n ", 4))
+		arg = ft_strnstr(tmp, "-n ", 4) + 3;
+	if (n == false)
+		ft_putendl_fd(arg, 1);
+	else
+		ft_putstr_fd(arg, 1);
+}
--- minishell.h
+++ minishell.h
@@ -14,6 +14,7 @@
 # define MINISHELL_H
 
 // INCLUDES
+# include "libft/libft.h"
 # include <stdio.h>
 # include <stdbool.h>
 # include <readline/readline.h>
