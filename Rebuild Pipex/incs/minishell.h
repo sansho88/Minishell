69d5b56a7325805c04a297ce3d7103bd54c42ebc
Robin <geral.robinjeu@gmail.com>
2022-05-05 14:36:56 +0200

    Restruc bis

---
+++ Rebuild Pipex/incs/minishell.h
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgriffit <tgriffit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/19 15:10:33 by tgriffit          #+#    #+#             */
+/*   Updated: 2022/05/03 17:00:30 by tgriffit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+// INCLUDES
+# include "libft/libft.h"
+# include <stdio.h>
+# include <stdbool.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+//Defines
+# define NOT_REDIR 0
+# define PIPE 1
+# define REDIR_TO_OUT 2
+# define CONCAT_TO_OUT 3
+# define REDIR_TO_IN 4
+# define CONCAT_TO_IN 5
+
+//Structs
+typedef struct s_argmode{
+	char	*arg;
+	int		mode;
+}	t_argmode;
+
+// FUNCTIONS
+
+void	rl_clear_history(void);
+
+// COLORS
+# define BBLU	"\033[1;34m"
+# define BGREEN	"\033[1;32m"
+# define BRED	"\033[1;31m"
+# define BLU	"\033[0;34m"
+# define GRN	"\033[0;32m"
+# define RED	"\033[0;31m"
+# define RST	"\033[0m"
+# define YLW	"\033[93m"
+
+
+#endif
---
+++ Rebuild Pipex/srcs/parsing/parse_redirecs.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_redirecs.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgeral <rgeral@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/03 17:38:42 by tgriffit          #+#    #+#             */
+/*   Updated: 2022/05/05 14:26:24 by rgeral           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incs/minishell.h"
+
+size_t	ft_cnt_redirecs(const char *line)
+{
+	size_t		count;
+	size_t		i;
+	bool		is_in_quotes;
+
+	count = 0;
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\"')
+			is_in_quotes = !is_in_quotes;
+		if (is_in_quotes == false)
+		{
+			if (line[i] == '>' && line[i + 1] && line[i + 1] != '>')
+				count++;
+			else if (line[i] == '<' && line[i + 1] && line[i + 1] != '<')
+				count++;
+		}
+		i++;
+	}
+	return (count);
+}
+
+/**
+ * Check if the nexts 2 chars are redirection characters, and returns the mode
+ * @param cmdline = A pointer to the first char to check
+ * @return The "mode" (check minishell.h)
+ */
+int	ft_check_redir(const char *cmdline)
+{
+	if (!cmdline)
+		return (NOT_REDIR);
+	if (*cmdline == '|' && *(cmdline + 1))
+		return (PIPE);
+	else if (*cmdline == '>' && *(cmdline + 1) && *(cmdline + 1) != '>')
+		return (REDIR_TO_OUT);
+	else if (*cmdline == '>' && *(cmdline + 1) && *(cmdline + 1) == '>')
+		return (CONCAT_TO_OUT);
+	else if (*cmdline == '<' && *(cmdline + 1) && *(cmdline + 1) != '<')
+		return (REDIR_TO_IN);
+	else if (*cmdline == '<' && *(cmdline + 1) && *(cmdline + 1) == '<')
+		return (CONCAT_TO_IN);
+	else
+		return (NOT_REDIR);
+}
+
+t_argmode	ft_fill_argmode_array(char *cmdline, size_t i)
+{
+	char		**tmp_split;
+	int			tmp_splitlen;
+	t_argmode	argmode_array;
+
+	tmp_split = ft_split_len(cmdline, *(cmdline + i), &tmp_splitlen);
+	argmode_array.arg = tmp_split[0];
+	argmode_array.mode = ft_check_redir(cmdline + i);
+	while (tmp_splitlen >= 0)
+		free(tmp_split[tmp_splitlen--]);
+	free(tmp_split);
+	return (argmode_array);
+}
+
+/**
+ * Take all the args and checks if each couple of chars is a redirect.
+ *
+ * @param cmdline = The cmdline sent by the user.
+ * @param argc = Will takes the value of numbers of args between each redirect.
+ * @return A t_argmode struct which contains each arg and the "mode" associated
+ * (check minishell.h)
+ */
+t_argmode	*split_arg_redirect(char *cmdline, int *argc)
+{
+	t_argmode	*res;
+	size_t		i;
+
+	res = malloc(ft_cnt_redirecs(cmdline) * sizeof(t_argmode *));
+	i = 0;
+	*argc = 0;
+	while (cmdline[i])
+	{
+		if (ft_check_redir(cmdline + i) != 0)
+		{
+			res[*argc] = ft_fill_argmode_array(cmdline, i);
+			(*argc)++;
+		}
+		else if (!ft_strchr(cmdline + i, '>') && !ft_strchr(cmdline + i, '<')
+			&& !ft_strchr(cmdline + i, '|'))
+		{
+			res[*argc].arg = cmdline + i;
+			res[*argc].mode = 0;
+		}
+		i++;
+	}
+	return (res);
+}
