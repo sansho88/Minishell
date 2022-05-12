dcd60e2a699bb114dbcb6801f5833d1f1ee0f950
Robin <geral.robinjeu@gmail.com>
2022-05-12 19:31:22 +0200

    multipipe v1

--- srcs/exec/sorting_hub.c
+++ srcs/exec/sorting_hub.c
@@ -6,20 +6,40 @@
 /*   By: rgeral <rgeral@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 18:57:33 by rgeral            #+#    #+#             */
-/*   Updated: 2022/05/12 17:57:21 by rgeral           ###   ########.fr       */
+/*   Updated: 2022/05/12 19:09:12 by rgeral           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incs/minishell.h"
 
+void	counting_redirections_fwd(t_args *d, t_argmode *argv)
+{
+	int i;
+	
+	i = d->acutal_arg;
+	while (argv[i].mode == 2)
+	{
+		i++;
+		d->count++;
+	}
+}
+
 void	sorting_hub(t_args *d, t_argmode *argv)
 {
 	d->j = 0;
+	d->count = 0;
 	while (d->acutal_arg < d->argc)
 	{
+		dprintf(2, "valeur actuelle de acutal arg : %d\n", d->acutal_arg);
 		fork_process(d, argv);
-		d->j++;
+		counting_redirections_fwd(d, argv);
+		if (d->count != 0)
+		{
+			d->acutal_arg = d->acutal_arg + d->count;
+			d->count = 0;
+		}
 		d->acutal_arg++;
+		d->j++;
 	}
 	
 }
