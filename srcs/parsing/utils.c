9a7f517227d07ae43f99426e6508f4c72827b1d9
TGriffit <tgriffit@student.42lyon.fr>
2022-09-28 19:23:07 +0200

    Little cleanup for norm echo "|;|>|<" is able to ignore the chars in Todo: Don't take anymore redirection between quotes

--- srcs/parsing/utils.c
+++ srcs/parsing/utils.c
@@ -6,7 +6,7 @@
 /*   By: tgriffit <tgriffit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 13:48:29 by tgriffit          #+#    #+#             */
-/*   Updated: 2022/09/27 17:14:05 by tgriffit         ###   ########.fr       */
+/*   Updated: 2022/09/28 16:38:21 by tgriffit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,13 +43,6 @@ void	free_t_argmode(t_argmode *args, size_t nb_args)
 	free(args);
 }
 
-bool    str_in_quotes(const char *str, size_t lenstr, bool is_env_var)
-{
-    if (!(str - is_env_var) || *(str - is_env_var) != '\'')
-        return (false);
-    return (str[lenstr] + 1 && str[lenstr] + 1 == '\'');
-}
-
 char	*ft_strstrchr(char *target, char **tab, size_t len_target)
 {
 	int		i;
@@ -76,86 +69,24 @@ char	*ft_strstrchr(char *target, char **tab, size_t len_target)
 	return (result);
 }
 
-char    *ft_strreplace(char *str, char *to_insert, int pos, int len_to_replace)
+char	*ft_strreplace(char *str, char *to_insert, int pos, int len_to_replace)
 {
-	char    *result;
-	size_t  size;
+	char			*result;
+	const size_t	size = ft_strlen(str) - len_to_replace \
+													+ ft_strlen(to_insert) + 1;
 
-    size = ft_strlen(str) - len_to_replace + ft_strlen(to_insert) + 1;
 	result = malloc(size);
 	if (!result)
 		return (NULL);
 	ft_strlcpy(result, str, pos + 1);
-    if (str_in_quotes(to_insert, len_to_replace + 1, true))
-        ft_strjoin_free(result, str + pos, 1);
-    else
-    {
-        ft_strlcat(result + pos, to_insert, size);
-        ft_strlcat(result + pos + ft_strlen(to_insert), str + pos
-                                                        + len_to_replace, size);
-    }
+	if (is_envar_in_sngl_quotes(to_insert, len_to_replace + 1, true))
+		ft_strjoin_free(result, str + pos, 1);
+	else
+	{
+		ft_strlcat(result + pos, to_insert, size);
+		ft_strlcat(result + pos + ft_strlen(to_insert), str + pos
+			+ len_to_replace, size);
+	}
 	free(str);
 	return (result);
-}
-
-/**
- *
- * @param str Start of the part of the str to test
- * @return the pos of the end's word, or the NULL terminating char
- */
-char *get_next_valid_sep(char *str)
-{
-    size_t  i;
-
-    i = 0;
-    while (str[i])
-    {
-    	if (!ft_isalnum(str[i]) && str[i] != '?' && str[i] != '_')
-            return (&str[i]);
-        i++;
-    }
-    return (NULL);
-}
-
-char *get_env_var(char *cmd, const char *start, size_t len_to_end, char **env)
-{
-	char *env_var;
-	char *tmp;
-
-	env_var = "";
-	tmp = ft_strstrchr((char *)start, env, len_to_end - 1);
-	if (tmp && !is_str_in_quotes(cmd, start, start + len_to_end, '\''))
-		env_var = tmp;
-	else if (tmp)
-		env_var = ft_strndup((char *)start - 1, get_next_valid_sep(start + 1) - start + 1);
-	return (env_var);
-}
-
-char	*replace_dollars(char *cmdline, char **env)
-{
-	char	*env_var;
-	char	*next_$;
-	char	*next_sep;
-	int 	offset;
-	int 	len;
-
-	next_$ = ft_strchr(cmdline, '$');
-	while (next_$)
-	{
-		next_sep = get_next_valid_sep(next_$ + 1);
-        offset = (int)(next_$ - cmdline);
-		if (next_sep)
-		{
-			len = (int)(next_sep - next_$);
-			env_var = get_env_var(cmdline, next_$ + 1, next_sep - next_$ - 1, env);
-		}
-		else
-		{
-			len = (int)(ft_strlen(cmdline) - offset);
-			env_var = ft_strstrchr(next_$ + 1, env, ft_strlen(next_$ + 1));
-		}
-		cmdline = ft_strreplace(cmdline, env_var, offset, len);
-		next_$ = ft_strchr(cmdline + offset + ft_strlen(env_var), '$');
-	}
-	return (cmdline);
 }
